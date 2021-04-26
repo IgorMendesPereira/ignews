c2078aee11d4d7d1fc91ec187b863d021e41ad6f
IgorMendesPereira <igor_mendes_pereira@outlook.com>
2021-04-26 09:24:17 -0300

    react, ts, next, sass

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import styles from '../styles/home.module.scss'
+
+export default function Home() {
+  return (
+    <>
+       <Head>
+         <title>Ig.News</title>
+        </Head>
+        <h1> Hello Mundo </h1>
+    </>
+  )
+}
